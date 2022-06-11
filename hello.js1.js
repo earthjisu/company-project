1d8be0f7a71c2e7ccfc5d9241e8ce84311b08938
Jisu Yu <yjs4882@naver.com>
2022-06-11 15:55:34 +0900

    first commit

---
+++ hello.js1.js
@@ -0,0 +1,24 @@
+age=prompt('나이를 입력하세요');
+n=parseInt(age);
+n=n+3;
+document.write('3년 뒤 나이는 '+n+'세 입니다');
+
+// x=1;
+// if(3>5){
+//     x=x+3;
+// } else{
+//     x=x+5;
+// }
+// x=x+4;
+// document.write(x);
+
+// menu={};
+// mname=prompt('메뉴명을 입력하세요');
+// while(mname!=''){
+//     price=prompt('가격을 입력하세요');
+//     menu[mname]=price;
+//     mname=prompt('메뉴명을 입력하세요');
+// }
+// for(key in menu){
+//     document.write(key+'  '+menu[key]+'<br>');
+// }
